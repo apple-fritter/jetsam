73f5447fc1f1e1a3d736fcaf649d083c349ff8b1
Ａｐｐｌｅ Ｆｒｉｔｔｅｒ <125221355+apple-fritter@users.noreply.github.com>
2023-06-13 14:44:28 -0700

    Update main.rs

--- source/main.rs
+++ source/main.rs
@@ -1,41 +1,48 @@
 use std::env;
 use std::fs::{self, File};
 use std::io::{self, BufRead, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use chrono::Local;
 
+const DELIMITER: &str = "☕";
+
 fn main() {
-    // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
-    // Check if two arguments (log directory path and wordlist path) are provided
-    if args.len() != 3 {
-        println!("Please provide the log directory path and wordlist path.");
-        return;
-    }
+    // Prompt for arguments if not provided
+    let log_directory = if args.len() > 1 {
+        PathBuf::from(&args[1])
+    } else {
+        println!("Please enter the path to the log directory:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok();
+        PathBuf::from(input.trim())
+    };
 
-    // Read the log directory path and wordlist path
-    let log_directory_path = &args[1];
-    let wordlist_path = &args[2];
+    let wordlist_path = if args.len() > 2 {
+        PathBuf::from(&args[2])
+    } else {
+        println!("Please enter the path to the wordlist file or directory:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok();
+        PathBuf::from(input.trim())
+    };
 
-    // Process the log files recursively
-    process_log_directory(Path::new(log_directory_path), wordlist_path);
+    // Process log files
+    process_log_directory(&log_directory, &wordlist_path);
 
-    println!("Jetsam operation completed.");
+    println!("Jetsam complete.");
 }
 
-fn process_log_directory(log_directory_path: &Path, wordlist_path: &str) {
-    // Iterate over the entries in the log directory
-    if let Ok(entries) = fs::read_dir(log_directory_path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                let path = entry.path();
-                if path.is_dir() {
-                    // Recursively process subdirectories
-                    process_log_directory(&path, wordlist_path);
-                } else if let Some(extension) = path.extension() {
-                    if extension == "txt" {
-                        // Process text log files
+fn process_log_directory(log_directory: &PathBuf, wordlist_path: &PathBuf) {
+    if log_directory.is_dir() {
+        if let Ok(entries) = fs::read_dir(log_directory) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_dir() {
+                        process_log_directory(&path, wordlist_path);
+                    } else if path.extension().unwrap_or_default() == "txt" {
                         process_log_file(&path, wordlist_path);
                     }
                 }
@@ -44,88 +51,100 @@ fn process_log_directory(log_directory_path: &Path, wordlist_path: &str) {
     }
 }
 
-fn process_log_file(log_file_path: &Path, wordlist_path: &str) {
-    // Open the log file
+fn process_log_file(log_file_path: &PathBuf, wordlist_path: &PathBuf) {
+    let wordlist: Vec<String> = load_wordlist(wordlist_path);
+
     if let Ok(file) = File::open(log_file_path) {
         let reader = io::BufReader::new(file);
+        let mut line_number = 0;
 
-        // Read the lines of the log file
-        for (line_number, line) in reader.lines().enumerate() {
+        for line in reader.lines() {
+            line_number += 1;
             if let Ok(line_content) = line {
-                if is_line_modified(&line_content, wordlist_path) {
-                    // Modify the line by adding # in column 1
-                    let modified_line = format!("#{}{}", &line_content[1..], '\n');
+                let line_fields: Vec<&str> = line_content.split(DELIMITER).collect();
+                if line_fields.len() >= 6 {
+                    let sender = line_fields[4];
+                    let message = line_fields[5];
 
-                    // Log the modification
-                    log_modification(log_file_path, line_number + 1, &line_content, &modified_line);
-                }
-            }
-        }
-    }
-}
+                    if wordlist.iter().any(|word| message.contains(word)) {
+                        // Sanitize the line by adding a # in column 1
+                        let sanitized_line = format!("#{}{}", DELIMITER, line_content);
 
-fn is_line_modified(line: &str, wordlist_path: &str) -> bool {
-    // Read the wordlist file(s) recursively
-    let wordlist_path = Path::new(wordlist_path);
+                        // Log the sanitized line to the Jetsam log
+                        let timestamp = Local::now().format("%Y%m%d-%H%M%S").to_string();
+                        let log_content = format!(
+                            "Timestamp: {}\nPath: {}\nLine Number: {}\nOriginal Line: {}\n",
+                            timestamp,
+                            log_file_path.display(),
+                            line_number,
+                            line_content
+                                .replace(DELIMITER, " ")
+                                .replace(" ", "")
+                        );
+                        let log_filename = format!("jetsam{}.log", timestamp);
+                        let log_path = dirs::home_dir()
+                            .unwrap_or_else(|| PathBuf::from("."))
+                            .join(&log_filename);
 
-    if wordlist_path.is_file() {
-        if let Ok(file) = File::open(wordlist_path) {
-            let reader = io::BufReader::new(file);
-            for word in reader.lines() {
-                if let Ok(word) = word {
-                    if line.contains(&word) {
-                        return true;
+                        // Write the log content to the logfile
+                        if let Ok(mut file) = File::create(&log_path) {
+                            file.write_all(log_content.as_bytes()).ok();
+                            println!("Jetsam log created at: {}", log_path.display());
+                        } else {
+                            println!("Failed to create Jetsam log at: {}", log_path.display());
+                        }
+
+                        // Modify the line in the log file
+                        if let Ok(mut file) = fs::OpenOptions::new().write(true).open(log_file_path) {
+                            let modified_line = format!("#{}{}", DELIMITER, sanitized_line);
+                            let _ = file.write_fmt(format_args!(
+                                "{}\n",
+                                modified_line
+                            ));
+                        }
                     }
                 }
             }
         }
-    } else if wordlist_path.is_dir() {
+    } else {
+        println!("Failed to open log file: {}", log_file_path.display());
+    }
+}
+
+fn load_wordlist(wordlist_path: &PathBuf) -> Vec<String> {
+    let mut wordlist: Vec<String> = Vec::new();
+
+    if wordlist_path.is_dir() {
         if let Ok(entries) = fs::read_dir(wordlist_path) {
             for entry in entries {
                 if let Ok(entry) = entry {
                     let path = entry.path();
-                    if let Some(extension) = path.extension() {
+                    if path.is_file() {
+                        let extension = path.extension().unwrap_or_default();
                         if extension == "txt" {
-                            if is_line_modified(line, path.to_str().unwrap()) {
-                                return true;
+                            if let Ok(file) = File::open(&path) {
+                                let reader = io::BufReader::new(file);
+                                for line in reader.lines() {
+                                    if let Ok(word) = line {
+                                        wordlist.push(word.trim().to_string());
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    } else if wordlist_path.is_file() {
+        if let Ok(file) = File::open(wordlist_path) {
+            let reader = io::BufReader::new(file);
+            for line in reader.lines() {
+                if let Ok(word) = line {
+                    wordlist.push(word.trim().to_string());
+                }
+            }
+        }
     }
 
-    false
-}
-
-fn log_modification(log_file_path: &Path, line_number: usize, line_content: &str, modified_line: &str) {
-    // Create the jetsam log file path
-    let home_dir = match dirs::home_dir() {
-        Some(path) => path,
-        None => return,
-    };
-
-    let log_file_name = format!("jetsam{}.log", Local::now().format("%y%m%d-%H%M%S"));
-    let jetsam_log_file_path = home_dir.join(log_file_name);
-
-    // Open the jetsam log file in append mode
-    let mut jetsam_log_file = match File::create(&jetsam_log_file_path) {
-        Ok(file) => file,
-        Err(_) => return,
-    };
-
-    // Write the modification details to the jetsam log file
-    let log_message = format!(
-        "{}\n{}\nLine {}\n{}\n{}\n\n",
-        Local::now().format("%Y%m%d-%H%M%S"),
-        log_file_path.display(),
-        line_number,
-        line_content.replace("☕", " "),
-        modified_line.replace("☕", " ")
-    );
-
-    if let Err(_) = jetsam_log_file.write_all(log_message.as_bytes()) {
-        return;
-    }
+    wordlist
 }
